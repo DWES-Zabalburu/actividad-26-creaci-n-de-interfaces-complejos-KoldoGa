844ce344a2a9b182d612687e39daefa3659d07ae
KoldoGa <koldo.garcia.santos@zabalburu.org>
2025-04-08 13:56:33 +0200

    Ejercicio Hecho

---
+++ Actividad26/src/main/java/org/zabalburu/daw1/actividad26/Actividad26.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.zabalburu.daw1.actividad26;
+
+import org.zabalburu.daw1.actividad26.vistas.Mi_Perfil;
+
+/**
+ *
+ * @author DAW1
+ */
+public class Actividad26 {
+
+    public static void main(String[] args) {
+        new Mi_Perfil().setVisible(true);
+    }
+}
